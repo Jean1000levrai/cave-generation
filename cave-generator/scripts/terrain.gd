139cf01cdf9400dc82c9ff5b334dd2ce882ce9f7
Jean1000 <jean2salomon@gmail.com>
2026-04-20 23:36:33 +0200

    correct structure

--- cave-generator/scripts/terrain.gd
+++ cave-generator/scripts/terrain.gd
@@ -1,13 +1,27 @@
 @tool
 extends MeshInstance3D
 
-@export var size := 256.0
+@export var grid_size := 256.0:
+	set(new_grid_size):
+		grid_size = new_grid_size
+		update_mesh()
+@export var iso_level := 0.0:
+	set(new_iso_level):
+		iso_level = new_iso_level
+		update_mesh()
 
 @export_range(4, 256, 4) var resolution := 32:
 	set(new_resolution):
 		resolution = new_resolution
 		update_mesh()
-
+@export var noise_seed := 0:
+	set(new_seed):
+		noise_seed = new_seed
+		update_mesh()
+@export var frequency := 0.1:
+	set(new_frequency):
+		frequency = new_frequency
+		update_mesh()
 @export var noise: FastNoiseLite:
 	set(new_noise):
 		noise = new_noise
@@ -15,18 +29,28 @@ extends MeshInstance3D
 		if noise:
 			noise.changed.connect(update_mesh)
 
-@export_range(4.0, 128.0, 4.0) var height := 64.0:
-	set(new_height):
-		height = new_height
-		update_mesh()
+var density_field = []   # 3D data storage
+var vertices = []
+var indices = []
+
+func generate_density_field() -> void:
+	pass
+
+func marching_cubes() -> void:
+	pass
+
+func generate_mesh() -> void:
+	pass
 
 func update_mesh() -> void:
-	var plane := PlaneMesh.new()
-	plane.subdivide_depth = resolution
-	plane.subdivide_width = resolution
-	plane.size = Vector2(size, size)
-	
-	var plane_array :=  plane.get_mesh_arrays()
-	var array_mesh := ArrayMesh.new()
-	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, plane_array)
-	mesh = array_mesh
+	vertices.clear()
+	indices.clear()
+
+	# 1. gen density field
+	generate_density_field()
+
+	# 2. marching cubes
+	marching_cubes()
+
+	# 3. build mesh
+	generate_mesh()
